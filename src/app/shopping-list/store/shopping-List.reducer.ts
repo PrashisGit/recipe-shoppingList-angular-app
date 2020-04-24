d82651ff3d090f516a7d738230f23168722d0d37
PrashisGit <prashisub@gmail.com>
2020-04-24 13:47:14 +0530

    installed NgRX Store package

---
+++ src/app/shopping-list/store/shopping-List.reducer.ts
@@ -0,0 +1,20 @@
+import { Action } from '@ngrx/store';
+
+import { Ingredient } from '../../shared/ingredient.model';
+
+const initalState = {
+  ingredients: [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10)
+  ]
+};
+
+export function shoppingListReducer(state = initalState, action: Action){
+  switch( action.type ){
+    case 'ADD_INGREDIENT' :
+      return {
+        ...state, // coppying old objects
+        Ingredient: [...state.ingredients, action] // overwritng the ingridents: first copy old date and dn adding new ingrident
+      };
+  }
+}
