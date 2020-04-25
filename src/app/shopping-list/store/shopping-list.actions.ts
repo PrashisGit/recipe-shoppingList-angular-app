629b45946e5e2115401a37fbf8b7e32047b2512d
PrashisGit <prashisub@gmail.com>
2020-04-25 22:30:40 +0530

    added uniq  name to action constant

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,12 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT_ITEM = 'START_EDIT_ITEM';
-export const STOP_EDIT_ITEM = 'STOP_EDIT_ITEM';
+export const ADD_INGREDIENT = '[Shopping List] ADD_INGREDIENT';
+export const ADD_INGREDIENTS = '[Shopping List] ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = '[Shopping List] UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = '[Shopping List] DELETE_INGREDIENT';
+export const START_EDIT_ITEM = '[Shopping List] START_EDIT_ITEM';
+export const STOP_EDIT_ITEM = '[Shopping List] STOP_EDIT_ITEM';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
