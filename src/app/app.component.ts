2cde424f5c790cad6a71301b341187a7d464c749
PrashisGit <prashisub@gmail.com>
2020-04-29 22:05:03 +0530

    added angular univaersal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { isPlatformBrowser} from '@angular/common';
 
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +13,19 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(private store: Store<fromApp.AppState>,
+              @Inject(PLATFORM_ID) private platformId,
+              private logginService: LoggingService) {}
 
   ngOnInit() {
-    this.store.dispatch( new AuthActions.AutoLogin());
+    // browser api will not work on Angular Universal, so in this project local storage will not work
+    // we used local storage in autologin
+    // to fix this we will check the platform, it will dispatch on browser and not in server
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch( new AuthActions.AutoLogin());
+    }
+
+    this.logginService.printLog('Hello from App component ngOnInit');
   }
 }
